07a224f215f368ab7e2c70af8e727c90a54421b2
Kasutan <contact@kasutan.pro>
2019-10-14 20:23:45 +0200

    course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,8 +7,6 @@ const Header = (props) =>  (
 )
 
 const Part = (props) => {
-	console.log(props)
-
 	return(
 		<p>{props.part.name} {props.part.exercises}</p>
 	)
@@ -16,42 +14,44 @@ const Part = (props) => {
 const Content = (props) => {
 	return(
 		<div>
-		<Part part={props.part1} />
-		<Part part={props.part2} />
-		<Part part={props.part3} />
+		<Part part={props.parts[0]} />
+		<Part part={props.parts[1]} />
+		<Part part={props.parts[2]} />
 		</div>
 	)
 	
 }
 
 const Total = (props) =>  (
-	<p>Number of exercises {props.total}</p>
+	<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 
 )
 const App = () => {
 	const course = 'Half Stack application development'
-	const part1 = {
+	const parts = [
+		{
 		name: 'Fundamentals of React',
 		exercises: 10
-	}
-	const part2 = {
+		},
+		{
 		name: 'Using props to pass data',
 		exercises: 7
-	}
-	const part3 = {
+		},
+		{
 		name: 'State of a component',
 		exercises: 14
-	}
+		}
+	]
 	
 	return (
 		<div>
 		<Header course={course} />
 
 
-		<Content part1={part1} part2={part2}  part3={part3} />
+		<Content parts={parts} />
 
 
-		<Total total={part1.exercises + part2.exercises + part3.exercises} />
+		<Total parts={parts}  />
 
 		</div>
 	)

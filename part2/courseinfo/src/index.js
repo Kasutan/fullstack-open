bc6c40ed9382316f1db63b8377078f93581cbc04
Kasutan <contact@kasutan.pro>
2019-11-03 20:21:51 +0100

    2.5: separate module

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,49 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './Course';
 
-const Header = (props) =>  (
-		<h2>{props.course}</h2>
-
-)
-
-const Part = (props) => {
-	return(
-		<p>{props.name} {props.exercises}</p>
-	)
-}
-const Content = ({parts}) => {
-	const rows = () => parts.map(part =>
-		<Part
-			key={part.id}
-			name={part.name}
-			exercises={part.exercises}
-		/>
-	)
-	return(
-		<div>
-			{rows()}
-		</div>
-	)
-	
-}
-
-const Total = ({parts}) => {
-	const exercises=parts.map(part => part.exercises)
-	const total = exercises.reduce( (accumulator, currentValue) => {
-		return accumulator + currentValue 
-		})
-	return(
-		<p><strong>Number of exercises {total}</strong></p>
-	)
-} 
-
-const Course = ({name, parts}) => (
-	<div>
-		<Header course={name} />
-		<Content parts={parts} />
-		<Total parts={parts} />
-	</div>
-)
 const App = () => {
 	const courses = [
 		{

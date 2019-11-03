2f841fece96e82d31f73dc84c4bb45886c75ad4a
Kasutan <contact@kasutan.pro>
2019-11-03 16:58:56 +0100

    Unicafe step 2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,22 +1,29 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import { isGenericTypeAnnotation } from '@babel/types';
 
 const App = () => {
 	// save clicks of each button to own state
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
+	const [all, setAll] = useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
+		setAll(all+1)
 	}
 
 	const handleNeutralClick = () => {
 		setNeutral(neutral + 1)
+		setAll(all+1)
+
 	}
 
 	const handleBadClick = () => {
 		setBad(bad + 1)
+		setAll(all+1)
+
 	}
 	
 	return (
@@ -30,8 +37,9 @@ const App = () => {
 			<p>Good {good}</p>
 			<p>Neutral {neutral}</p>
 			<p>Bad {bad}</p>
-
-
+			<p>All {all}</p>
+			<p>Average { (good - bad) / all }</p>
+			<p>Positive { 100 * good / all } %</p>
 		</div>
 	)
 	}

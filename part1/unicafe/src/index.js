6575c0721459ac46f18b1d2eca94e494275a42c4
Kasutan <contact@kasutan.pro>
2019-11-03 17:13:04 +0100

    unicafe step 4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,18 +10,26 @@ const Statistics = (props) => {
 		positive=100 * props.good /all
 	}
 	
+	if(props.given) {
+		return (
+			<div>
+				<p>Good {props.good}</p>
+				<p>Neutral {props.neutral}</p>
+				<p>Bad {props.bad}</p>
+				<p>All {all}</p>
+				<p>Average {average}</p>
+				<p>Positive {positive} %</p>
+			</div>
+		)
+	}
 
 	return (
 		<div>
-			<h2>Statistics</h2>
-			<p>Good {props.good}</p>
-			<p>Neutral {props.neutral}</p>
-			<p>Bad {props.bad}</p>
-			<p>All {all}</p>
-			<p>Average {average}</p>
-			<p>Positive {positive} %</p>
+			No feedback given
 		</div>
 	)
+
+	
 }
 
 	const App = () => {
@@ -29,19 +37,21 @@ const Statistics = (props) => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
+	const [given, setGiven] = useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
+		setGiven(1)
 	}
 
 	const handleNeutralClick = () => {
 		setNeutral(neutral + 1)
-
+		setGiven(1)
 	}
 
 	const handleBadClick = () => {
 		setBad(bad + 1)
-
+		setGiven(1)
 	}
 	
 	return (
@@ -50,8 +60,8 @@ const Statistics = (props) => {
 			<button onClick={handleGoodClick}>good</button>
 			<button onClick={handleNeutralClick}>neutral</button>
 			<button onClick={handleBadClick}>bad</button>
-
-			<Statistics good={good} neutral={neutral} bad={bad} />
+			<h2>Statistics</h2>
+			<Statistics good={good} neutral={neutral} bad={bad} given={given}/>
 		</div>
 	)
 	}

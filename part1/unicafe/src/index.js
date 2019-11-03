ad43901cbea643243cd8f8869e56cd4298237b08
Kasutan <contact@kasutan.pro>
2019-11-03 17:09:53 +0100

    unicafe step 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,46 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import { isGenericTypeAnnotation } from '@babel/types';
 
-const App = () => {
+const Statistics = (props) => {
+	const all=props.good + props.neutral + props.bad
+	let average = '-'
+	let positive= '-'
+	if(all>0) {
+		average=(props.good - props.bad)/all
+		positive=100 * props.good /all
+	}
+	
+
+	return (
+		<div>
+			<h2>Statistics</h2>
+			<p>Good {props.good}</p>
+			<p>Neutral {props.neutral}</p>
+			<p>Bad {props.bad}</p>
+			<p>All {all}</p>
+			<p>Average {average}</p>
+			<p>Positive {positive} %</p>
+		</div>
+	)
+}
+
+	const App = () => {
 	// save clicks of each button to own state
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
-	const [all, setAll] = useState(0)
 
 	const handleGoodClick = () => {
 		setGood(good + 1)
-		setAll(all+1)
 	}
 
 	const handleNeutralClick = () => {
 		setNeutral(neutral + 1)
-		setAll(all+1)
 
 	}
 
 	const handleBadClick = () => {
 		setBad(bad + 1)
-		setAll(all+1)
 
 	}
 	
@@ -33,13 +51,7 @@ const App = () => {
 			<button onClick={handleNeutralClick}>neutral</button>
 			<button onClick={handleBadClick}>bad</button>
 
-			<h2>Statistics</h2>
-			<p>Good {good}</p>
-			<p>Neutral {neutral}</p>
-			<p>Bad {bad}</p>
-			<p>All {all}</p>
-			<p>Average { (good - bad) / all }</p>
-			<p>Positive { 100 * good / all } %</p>
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	)
 	}

37e58babf335d77a7ea505045f190eec15ad0952
Kasutan <contact@kasutan.pro>
2019-11-25 12:49:51 +0100

    2.9*: The Phonebook Step4

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -4,10 +4,14 @@ import Persons from './Persons';
 
 const App = () => {
 const [ persons, setPersons] = useState([
-	{ name: 'Arto Hellas', number : '040-1234567'}
+	{ name: 'Arto Hellas', number: '040-123456' },
+	{ name: 'Ada Lovelace', number: '39-44-5323523' },
+	{ name: 'Dan Abramov', number: '12-43-234345' },
+	{ name: 'Mary Poppendieck', number: '39-23-6423122' }
 ]) 
 const [ newName, setNewName ] = useState('un nouveau nom')
 const [ newNumber, setNewNumber ] = useState('0033-')
+const [ newSearch, setNewSearch ] = useState('')
 
 const addName = (event) => {
 	event.preventDefault()
@@ -36,17 +40,21 @@ const handleNameChange = (event) => {
 const handleNumberChange = (event) => {
 	setNewNumber(event.target.value)
 }
+const handleSearchChange = (event) => {
+	setNewSearch(event.target.value)
+}
 
 return (
 	<div>
 	<h2>Phonebook</h2>
+	<div>filter shown with: <input value={newSearch}  onChange={handleSearchChange}/></div>
 	<form onSubmit={addName}>
 		<div>name: <input value={newName}  onChange={handleNameChange}/></div>
 		<div>number: <input value={newNumber}  onChange={handleNumberChange}/></div>
 		<div><button type="submit">add</button></div>
 	</form>
 	<h2>Numbers</h2>
-	<Persons persons={persons}/>
+	<Persons persons={persons} search={newSearch}/>
 	</div>
 )
 }
--- part2/thephonebook/src/Persons.js
+++ part2/thephonebook/src/Persons.js
@@ -6,13 +6,20 @@ return(
 	<p>{props.name} {props.number}</p>
 )
 }
-const Persons = ({persons}) => {
-const rows = () => persons.map(person =>
-	<Details
-		key={person.name}
-		name={person.name}
-		number={person.number}
-	/>
+const Persons = ({persons, search}) => {
+const rows = () => persons.map((person) => {
+	if(search==='' || person.name.toLowerCase().indexOf(search.toLowerCase())>=0) {
+		return(<Details
+			key={person.name}
+			name={person.name}
+			number={person.number}
+		/>)
+	} else {
+		return ('')
+	}
+}
+	
+	
 )
 return(
 	<div>
